e72b471895b74a4d0d406857849845bf7b1fb704
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-11-08 23:59:50 -0500

    created models and scripts files - controller not yet started

---
+++ scripts/date.js
@@ -0,0 +1,11 @@
+var createDate = function(){
+    var d = new Date();
+    var formatDate = "";
+
+    formatDate += (d.getMonth() + 1) + "_";
+    formatDate += d.getDate() + "_";
+    formatDate += d.getFullYear();
+
+    return formatDate;
+};
+module.exports = makeDate;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Requiring dependencies
 var express = require("express");
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var expressHandlebars = require("express-handlebars");
 
